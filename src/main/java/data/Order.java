9920d9e3af0b30cf75a425700064c00260e63a9f
KatyaSadykova <ThisIsKaterin@yandex.ru>
2025-01-12 18:17:23 +0300

    Diplom-2

--- src/main/java/data/Order.java
+++ src/main/java/data/Order.java
@@ -1,2 +1,20 @@
-package data;public class Order {
+package data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Order {
+    private List<String> ingredients;
+
+    public Order setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+        return this;
+    }
 }
--- src/main/java/order/OrderGenerator.java
+++ src/main/java/order/OrderGenerator.java
@@ -1,2 +1,11 @@
-package order;public class OrderGenerator {
+package order;
+
+import data.Order;
+
+public class OrderGenerator {
+    public static Order getListOrder() {
+
+        return new Order()
+                .setIngredients(OrderClient.getAllIngredients().extract().path("data._id"));
+    }
 }
